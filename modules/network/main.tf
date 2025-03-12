f4fcf9556634ae8f88945c5cb6b52f270dc8d9f2
mika <michel.gouzee@hotmail.com>
2025-03-12 09:45:47 +0100

    remove resource creation of vnet as it already exists

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,10 +1,9 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.virtual_network_name
-  address_space       = [var.vnet_address_space]
-  location            = var.rg_location
-  resource_group_name = var.rg_name
-
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = var.virtual_network_name
+#   address_space       = [var.vnet_address_space]
+#   location            = var.rg_location
+#   resource_group_name = var.rg_name
+# }
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
